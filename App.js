31cf60b88492b70d48b7057c335f4446c9cb7ed7
pvulnguyen <pvulnguyen@gmail.com>
2023-10-04 08:35:47 -0700

    add: selected image state

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import * as ImagePicker from 'expo-image-picker';
@@ -9,6 +10,8 @@ import ImageViewer from './components/ImageViewer';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const pickImageAsync = async () => {
     let res = await ImagePicker.launchImageLibraryAsync({
       // When true, users can crop the image during the selection process on
@@ -18,7 +21,7 @@ export default function App() {
     });
 
     if (!res.canceled) {
-      console.log(res);
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You did not select any image.');
     }
@@ -27,7 +30,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
         <Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
