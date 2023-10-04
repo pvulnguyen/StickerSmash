0a1d1f070d191a01ad2b119af53c68dd5b5f8c5f
pvulnguyen <pvulnguyen@gmail.com>
2023-10-04 08:32:04 -0700

    add: select image from library feature

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+
+import * as ImagePicker from 'expo-image-picker';
+import { StatusBar } from 'expo-status-bar';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -7,13 +9,28 @@ import ImageViewer from './components/ImageViewer';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let res = await ImagePicker.launchImageLibraryAsync({
+      // When true, users can crop the image during the selection process on
+      // Android and iOS but not on the web.
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!res.canceled) {
+      console.log(res);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme='primary' label='Choose a photo' />
+        <Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
         <Button label='Use this photo' />
       </View>
       <StatusBar style='auto' />
